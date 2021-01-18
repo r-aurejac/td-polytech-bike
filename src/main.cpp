44c01a1d250710ee895d0e72b4a292637893f249
raphael <raphael.aurejac@gmail.com>
2021-01-18 10:43:47 +0100

    ajout longue route

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "Road.h"
+#include "Bike.h"
+
+int main()
+{
+    Road road(20);
+    //road.show();
+    Bike bike;
+    bike.run(road);
+    Road longRoad(40);
+    bike.run(longRoad);
+    
+
+    return 0;
+}
